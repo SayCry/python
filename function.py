39731befa94fb110b0036a8fe7a6f8fa1fda2a3e
SayCayTOT <markchaya2547@gmail.com>
2022-07-26 14:33:19 +0700

    python

---
+++ function.py
@@ -0,0 +1,9 @@
+def hello():
+    print("Hello world")
+hello()
+
+def sum(x,y):
+    x+y
+    return  hello
+
+print(sum(5,8))
---
+++ work-function.py
@@ -0,0 +1,11 @@
+from math import pi
+
+
+def square(width,height):
+    return width*height
+def triangle(base,height):
+    return 0.5*base*height
+def circle(radius):
+    return pi*radius*radius
+
+#นายชยากร   หยางตระกูล ม.6/14 เลขที่ 4
